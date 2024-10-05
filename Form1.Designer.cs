44cf06c51b6b35a5b2868cefec5937f63819ddac
Student406-11 <Student406-11@NPK-102O-10>
2024-10-05 13:17:10 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textdate = new System.Windows.Forms.TextBox();
+            this.textboxGlav = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.date = new System.Windows.Forms.TextBox();
+            this.glav = new System.Windows.Forms.TextBox();
+            this.text = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(548, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(629, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Изменить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(548, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(629, 359);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textdate
+            // 
+            this.textdate.Location = new System.Drawing.Point(27, 330);
+            this.textdate.Name = "textdate";
+            this.textdate.Size = new System.Drawing.Size(100, 20);
+            this.textdate.TabIndex = 5;
+            this.textdate.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textboxGlav
+            // 
+            this.textboxGlav.Location = new System.Drawing.Point(27, 390);
+            this.textboxGlav.Name = "textboxGlav";
+            this.textboxGlav.Size = new System.Drawing.Size(100, 20);
+            this.textboxGlav.TabIndex = 6;
+            this.textboxGlav.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(167, 330);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(256, 90);
+            this.textBox3.TabIndex = 7;
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(27, 308);
+            this.date.Name = "date";
+            this.date.ReadOnly = true;
+            this.date.Size = new System.Drawing.Size(53, 20);
+            this.date.TabIndex = 8;
+            this.date.Text = "Дата";
+            this.date.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.date.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // glav
+            // 
+            this.glav.Location = new System.Drawing.Point(27, 364);
+            this.glav.Name = "glav";
+            this.glav.ReadOnly = true;
+            this.glav.Size = new System.Drawing.Size(70, 20);
+            this.glav.TabIndex = 9;
+            this.glav.Text = "Заголовок";
+            this.glav.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(167, 308);
+            this.text.Name = "text";
+            this.text.ReadOnly = true;
+            this.text.Size = new System.Drawing.Size(53, 20);
+            this.text.TabIndex = 10;
+            this.text.Text = "Текст";
+            this.text.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(-4, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(779, 302);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(429, 390);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(81, 20);
+            this.textBox5.TabIndex = 15;
+            this.textBox5.Text = "Слова";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(429, 418);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.glav);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textboxGlav);
+            this.Controls.Add(this.textdate);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textdate;
+        private System.Windows.Forms.TextBox textboxGlav;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox date;
+        private System.Windows.Forms.TextBox glav;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.IO;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        List<string[]> data = new List<string[]>();
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.Columns.Add("Дата", 100);
+            listView1.Columns.Add("Заголовок", 100);
+            listView1.Columns.Add("Текст", 550);
+            string filePath = "data.json";
+
+            if (File.Exists(filePath))
+            {
+                string jsonString = File.ReadAllText(filePath);
+
+                // Десериализуем данные из JSON
+                List<DataItem> dataItems = JsonSerializer.Deserialize<List<DataItem>>(jsonString);
+
+                // Заполняем ListView данными
+                foreach (DataItem item in dataItems)
+                {
+                    ListViewItem newItem = new ListViewItem(item.Date);
+                    newItem.SubItems.Add(item.Name);
+                    newItem.SubItems.Add(item.Text);
+                    listView1.Items.Add(newItem);
+                }
+                data = new List<string[]>();
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    string[] rowData = new string[item.SubItems.Count];
+
+                    // Заполняем массив данными из каждого подэлемента (столбца)
+                    for (int i = 0; i < item.SubItems.Count; i++)
+                    {
+                        rowData[i] = item.SubItems[i].Text;
+                    }
+                    // Добавляем массив данных в список
+                    data.Add(rowData);
+                }
+            }
+            }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textdate.Text != "" && textdate.Text != "Заполните поле")
+            {
+                if (textboxGlav.Text != "" && textboxGlav.Text != "Заполните поле")
+                {
+                    if (textBox3.Text != "" && textBox3.Text != "Заполните поле")
+                    {
+                        listView1.Items.Add(new ListViewItem(new string[] { $"{textdate.Text}", $"{textboxGlav.Text}", $"{textBox3.Text}" }));
+                    }
+                    else
+                        textBox3.Text = "Заполните поле";
+                }
+                else
+                    textboxGlav.Text = "Заполните поле";
+            }
+            else
+                textdate.Text = "Заполните поле";
+            data = new List<string[]>();
+            foreach (ListViewItem item in listView1.Items)
+            {
+                string[] rowData = new string[item.SubItems.Count];
+
+                // Заполняем массив данными из каждого подэлемента (столбца)
+                for (int i = 0; i < item.SubItems.Count; i++)
+                {
+                    rowData[i] = item.SubItems[i].Text;
+                }
+                // Добавляем массив данных в список
+                data.Add(rowData);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                if (textdate.Text != "" && textdate.Text != "Заполните поле")
+                {
+                    if (textboxGlav.Text != "" && textboxGlav.Text != "Заполните поле")
+                    {
+                        if (textBox3.Text != "" && textBox3.Text != "Заполните поле")
+                        {
+                            selectedItem.SubItems[0].Text = textdate.Text;
+                            selectedItem.SubItems[1].Text = textboxGlav.Text;
+                            selectedItem.SubItems[2].Text = textBox3.Text;
+                        }
+                        else
+                            textBox3.Text = "Заполните поле";
+                    }
+                    else
+                        textboxGlav.Text = "Заполните поле";
+                }
+                else
+                    textdate.Text = "Заполните поле";
+            }
+            data = new List<string[]>();
+            foreach (ListViewItem item in listView1.Items)
+            {
+                string[] rowData = new string[item.SubItems.Count];
+
+                // Заполняем массив данными из каждого подэлемента (столбца)
+                for (int i = 0; i < item.SubItems.Count; i++)
+                {
+                    rowData[i] = item.SubItems[i].Text;
+                }
+                // Добавляем массив данных в список
+                data.Add(rowData);
+            }
+        }
+
+        private void Click(object sender, MouseEventArgs e)
+        {
+            ListViewItem selectedItem = listView1.SelectedItems[0];
+            textdate.Text = selectedItem.Text;
+            textboxGlav.Text = selectedItem.SubItems[1].Text;
+            textBox3.Text = selectedItem.SubItems[2].Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                listView1.Items.Remove(selectedItem);
+                data = new List<string[]>();
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    string[] rowData = new string[item.SubItems.Count];
+
+                    // Заполняем массив данными из каждого подэлемента (столбца)
+                    for (int i = 0; i < item.SubItems.Count; i++)
+                    {
+                        rowData[i] = item.SubItems[i].Text;
+                    }
+                    // Добавляем массив данных в список
+                    data.Add(rowData);
+                }
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = textBox6.Text.ToLower();
+
+            // Фильтруем элементы ListView
+            listView1.Items.Clear();
+            if (searchText == "") // Проверка на пустое поле
+            {
+                // Если поле пустое, отображаем все данные
+                foreach (string[] row in data)
+                {
+                    ListViewItem newItem = new ListViewItem(row[0]);
+                    for (int i = 1; i < row.Length; i++)
+                    {
+                        newItem.SubItems.Add(row[i]);
+                    }
+                    listView1.Items.Add(newItem);
+                }
+            }
+            else
+            {
+                // Если поле не пустое, выполняем фильтрацию
+                foreach (string[] row in data)
+                {
+                    bool matchFound = false;
+                    for (int i = 0; i < row.Length; i++)
+                    {
+                        if (row[i].ToLower().Contains(searchText))
+                        {
+                            matchFound = true;
+                            break;
+                        }
+                    }
+
+                    if (matchFound)
+                    {
+                        ListViewItem newItem = new ListViewItem(row[0]);
+                        for (int i = 1; i < row.Length; i++)
+                        {
+                            newItem.SubItems.Add(row[i]);
+                        }
+                        listView1.Items.Add(newItem);
+                    }
+                }
+            }
+
+            // ОБНОВЛЕНИЕ ListView
+            listView1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            List<DataItem> dataItems = new List<DataItem>();
+            foreach (string[] row in data)
+            {
+                dataItems.Add(new DataItem
+                {
+                    Date = row[0],
+                    Name = row[1],
+                    Text = row[2]
+                });
+            }
+
+            // Сериализуем данные в JSON
+            string jsonString = JsonSerializer.Serialize(dataItems);
+
+            // Сохраняем JSON-строку в файл
+            string filePath = "data.json"; // Путь к файлу
+            File.WriteAllText(filePath, jsonString);
+        }
+    }
+    }
+    
